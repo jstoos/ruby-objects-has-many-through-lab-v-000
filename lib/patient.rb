6f86b63e7bbbdd2b0fa07a2c8dc1cbbb4e9d515c
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-17 16:48:57 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,26 +1,25 @@
 class Patient
 
-attr_accessor :name, :appointments, :appointment
+  attr_accessor :name, :appointments, :appointment
 
-def initialize(name)
-  @name = name
-  @appointments = []
-end
-
-def add_appointment(appointment)
-  @appointments << appointment
-  appointment.patient = self
-end
-
-def appointments
-  @appointments
-end
-
-def doctors
-  appointments.collect do |instance|
-    instance.doctor
+  def initialize(name)
+    @name = name
+    @appointments = []
   end
-end
 
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    appointments.collect do |instance|
+      instance.doctor
+    end
+  end
 
 end
