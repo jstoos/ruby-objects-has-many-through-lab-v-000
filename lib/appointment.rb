c99f527541501528e17a86e9a951c6f6c2cb822d
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-17 16:44:41 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,12 @@
 class Appointments
 
-attr_accessor :doctor
+  attr_accessor :doctor
 
-def initialize(date, doctor)
-  @date = date
-  @doctor = doctor
-  doctor.appointment = self
-  doctor.add_appointment(self)
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.appointment = self
+    doctor.add_appointment(self)
+  end
+
 end
